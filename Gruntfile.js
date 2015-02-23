fb3d9763b38be7a50a859283012bc5a89670656e
anibe <mail@anibeagamah.com>
2015-02-23 21:48:18 +0000

    Adding tasks and dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,18 +38,75 @@ module.exports = function ( grunt ) {
                     keepalive: true
                 }
             }
+        },
+
+        jasmine: {
+            dev: {
+                src: ['src/js/*.js'],
+                options: {
+                    vendor: 'src/js/lib/**/*.js',
+                    specs: 'src/spec/js/**/*.spec.js',
+                    keepRunner: true
+                }
+            }
+        },
+
+        clean: {
+            options: {
+                force: true
+            },
+            styles: {
+                src: [
+                    'src/css/main.css',
+                    'src/css/main.css.map'
+                ]
+            }
+        },
+
+        watch: {
+            options: {
+                spawn: false,
+                livereload: true
+            },
+            sass: {
+                files: ['src/css/**/*.scss'],
+                tasks: ['sass:expanded']
+            }
         }
 
     });
 
     grunt.loadNpmTasks( 'grunt-contrib-jshint' );
     grunt.loadNpmTasks( 'grunt-contrib-csslint' );
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks( 'grunt-contrib-connect' );
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
     // Runs all checkstyle stuff
     grunt.registerTask( 'default', [
         'jshint',
         'csslint'
     ] );
+
+    // tests
+    grunt.registerTask( 'test', [
+        'default',
+        'jasmine'
+    ] );
+
+    // Dev
+    grunt.registerTask( 'dev', [
+        'clean',
+        'sass:compressed',
+        'default',
+        'test',
+        'connect',
+        'watch'
+
+    ] );
+
+
 
 };
